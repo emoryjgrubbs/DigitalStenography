41d4740a0e1cfbeeec6aa6e4cb260eae146cb326
Emory Grubbs <emoryjgrubbs@gmail.com>
2024-10-30 16:11:46 -0500

    modified extraction method to write to a file

--- decode.py
+++ decode.py
@@ -2,7 +2,8 @@ from PIL import Image
 import sys
 
 
-def extract_message_from_image(image_path):
+def extract_message_from_image(image_path, output_path):
+    message_file = open(output_path, "wb")
     image = Image.open(image_path)
     pixels = image.load()
 
@@ -32,26 +33,155 @@ def extract_message_from_image(image_path):
 
                     # Check if the end of the message is reached
                     if binary_message.endswith(end_of_message):
-                        print(binary_to_text(binary_message[:-len(end_of_message)]))
-                        return
+                        # convert str to binary
+                        message_file.write(str_bin_to_bytes(binary_message[:-len(end_of_message)]))
+                        message_file.close()
+                        return 1  # hidden message writen to file
                     elif len(binary_message) == 512:
-                        print(binary_to_text(binary_message), end='')
+                        # convert str to binary
+                        message_file.write(str_bin_to_bytes(binary_message))
                         binary_message = ''
 
-    return "No hidden message found."
+    message_file.close()
+    return -1  # "No hidden message found."
 
 
-def binary_to_text(binary_message):
-    chars = [binary_message[i:i+8] for i in range(0, len(binary_message), 8)]
-    return ''.join([chr(int(char, 2)) for char in chars])
+def str_bin_to_bytes(str_str):
+    if (len(str_str) % 8) != 0:
+        # error
+        return
+    str_bytes = bytearray()  # empty byte string
+    for i in range(0, int(len(str_str)/8), 8):
+        cur_int = 0
+        if str_str[i] == '1':
+            cur_int += 128
+        if str_str[i+1] == '1':
+            cur_int += 64
+        if str_str[i+2] == '1':
+            cur_int += 32
+        if str_str[i+3] == '1':
+            cur_int += 16
+        if str_str[i+4] == '1':
+            cur_int += 8
+        if str_str[i+5] == '1':
+            cur_int += 4
+        if str_str[i+6] == '1':
+            cur_int += 2
+        if str_str[i+7] == '1':
+            cur_int += 1
+        cur_byte = cur_int.to_bytes(1)
+        str_bytes.extend(cur_byte)
+        # add the character in binay form to the end of bin
+    return str_bytes
+
+
+# status -6 too many args
+# status -5 : -4 too few args
+# status -3 : -2 overloaded input
+# status -1 undefined behavior flag given as image/message
+# status 0 nothing happened
+# status 1 all good, secret message should be writen to specified file
+# status 2 all good, secret message should be printed
+def handle_argv(argv):
+    status = 0
+    force = False
+    input_file = ""
+    output_file = ""
+    unflagged_args = []
+    current_flag = ""
+    for arg in argv:
+        # if there is a current flag & arg is a flag
+        if current_flag != "" and arg[0:1] == '-':
+            return [-1]
+        match arg:
+            # flags
+            case '--input':
+                current_flag = 'output image'
+            case '-i':
+                current_flag = 'output image'
+            case '--output':
+                current_flag = 'output image'
+            case '-o':
+                current_flag = 'output image'
+            case '--print':
+                status = 2
+                output_file = 'temp.txt'
+            case '-p':
+                status = 2
+                output_file = 'temp.txt'
+            case '-f':
+                force = True
+            # input arg
+            case _:
+                if current_flag != "":
+                    match current_flag:
+                        case 'input image':
+                            if input_file != "":
+                                return [-2]
+                            else:
+                                input_file = arg
+                        case 'output image':
+                            if output_file != "":
+                                return [-3]
+                            else:
+                                output_file = arg
+                    current_flag = ""
+                else:
+                    unflagged_args.append(arg)
+
+    # handle unflagged args
+    if input_file == "":
+        if len(unflagged_args) > 0:
+            input_file = unflagged_args[0]
+            unflagged_args = unflagged_args[1:len(unflagged_args)]
+        else:
+            return [-4]
+    if output_file == "":
+        if len(unflagged_args) > 0:
+            output_file = unflagged_args[0]
+            unflagged_args = unflagged_args[1:len(unflagged_args)]
+        else:
+            return [-5]
+
+    if len(unflagged_args) == 0:
+        return [status, input_file, output_file, force]
+    else:
+        return [-6]
 
 
 def main():
-    if len(sys.argv) == 2:
-        image_path = sys.argv[1]
-        extract_message_from_image(image_path)
-    else:
-        print("No Image Path Provided.")
+    standardized_argv = handle_argv(sys.argv[1:len(sys.argv)])
+    status = standardized_argv[0]
+    if status > 0:
+        input_file = standardized_argv[1]
+        output_file = standardized_argv[2]
+        force = standardized_argv[3]
+
+    match status:
+        case -6:
+            print("Error, Too Many Arguments Provided")
+        case -5:
+            print("Error, No Output File Provided")
+        case -4:
+            print("Error, No Input File Provided")
+        case -3:
+            print("Error, Multiple Output Files Provided")
+        case -2:
+            print("Error, Multiple Input Files Provided")
+        case -1:
+            print("No Image Path Provided.")
+        case 1:
+            if os.path.isfile(output_file) and not force:
+                print("Warning, Output File Exists\nOverwrite? Specify -f")
+            else:
+                extract_message_from_image(input_file, output_file)
+                print("Success, Message writen to file: ", output_file)
+        case 2:
+            extract_message_from_image(input_file, output_file)
+            # call fun to print all string text from file
+        case _:
+            print("Error, Unknown Error")
+
 
 
 if __name__ == "__main__":
