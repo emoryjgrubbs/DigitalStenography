d2a79287281786b013217e44b640c31f214574b9
Emory Jackson Grubbs <104403182+emoryjgrubbs@users.noreply.github.com>
2024-11-21 04:02:23 -0600

    Pyqt (#2)
    * basic qt gui
    
    * added message icons
    
    * changed browse button from text button to action inside field
    
    * added additional labels for inputs
    
    * fixed the lsb_hide/extract when not in the directory

--- stegosaurus.py
+++ stegosaurus.py
@@ -1,203 +1,192 @@
 from subprocess import Popen, PIPE
-import tkinter as tk
-from tkinter import messagebox  # for print decode
-from tkinter import filedialog  # for selecting files
+import sys
+import os
+from PyQt6.QtCore import Qt
+import PyQt6.QtWidgets as qtw
+import PyQt6.QtGui as qtg
 
 
-def clear_text_feilds():
-    input_img_entry.delete(0, 'end')
-    output_img_entry.delete(0, 'end')
-    input_txt_entry.delete(0, 'end')
-    output_txt_entry.delete(0, 'end')
+class Stegosaurus(qtw.QWidget):
+    def __init__(self):
+        super().__init__()
+        # title
+        self.setWindowTitle("Stegosaurus")
+
+        # layout
+        self.setLayout(qtw.QVBoxLayout())
+
+        # icons
+        pixmapi = getattr(qtw.QStyle.StandardPixmap, "SP_DirIcon")
+        browse_icon = self.style().standardIcon(pixmapi)
+
+        # method title
+        method_lbl = qtw.QLabel("Least Significant Bit - Stegenography")
+        method_lbl.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        method_lbl.setFont(qtg.QFont('Helvetica', 16))
+
+        # mode tab
+        mode_tab = qtw.QTabWidget()
+
+        # hide mode page
+        hide_pge = qtw.QWidget(mode_tab)
+        hide_pge.setLayout(qtw.QFormLayout())
+        mode_tab.addTab(hide_pge, "Hide Message")
+        # hide message inputs
+        hide_cover_lbl = qtw.QLabel("Cover Image:")
+        hide_pge.layout().addRow(hide_cover_lbl)
+        hide_cover_ent = qtw.QLineEdit()
+        hide_cover_ent.setPlaceholderText("Enter the path to the cover image")
+        hide_cover_dialog = hide_cover_ent.addAction(browse_icon, hide_cover_ent.ActionPosition(1))
+        hide_cover_dialog.triggered.connect(lambda: get_path("hide_cover"))
+        hide_pge.layout().addRow(hide_cover_ent)
+
+        hide_stego_lbl = qtw.QLabel("Stego Image:")
+        hide_pge.layout().addRow(hide_stego_lbl)
+        hide_stego_ent = qtw.QLineEdit()
+        hide_stego_ent.setPlaceholderText("Enter location to save the stego image")
+        hide_stego_dialog = hide_stego_ent.addAction(browse_icon, hide_stego_ent.ActionPosition(1))
+        hide_stego_dialog.triggered.connect(lambda: get_path("hide_stego"))
+        hide_pge.layout().addRow(hide_stego_ent)
+
+        hide_txt_lbl = qtw.QLabel("Message File:")
+        hide_pge.layout().addRow(hide_txt_lbl)
+        hide_txt_ent = qtw.QLineEdit()
+        hide_txt_ent.setPlaceholderText("Enter the path to the message text file")
+        hide_txt_dialog = hide_txt_ent.addAction(browse_icon, hide_txt_ent.ActionPosition(1))
+        hide_txt_dialog.triggered.connect(lambda: get_path("hide_txt"))
+        hide_pge.layout().addRow(hide_txt_ent)
+
+        hide_str_ent = qtw.QTextEdit()
+        hide_str_ent.setPlaceholderText("Enter the message to hide")
+        # hide message submit
+        hide_btn = qtw.QPushButton("Submit", clicked=lambda: handle_submit("hide_file"))
+        hide_pge.layout().addRow(hide_btn)
+
+        # extract mode page
+        extract_pge = qtw.QWidget(mode_tab)
+        extract_pge.setLayout(qtw.QFormLayout())
+        mode_tab.addTab(extract_pge, "Extract Message")
+        # extract message inputs
+        extract_stego_lbl = qtw.QLabel("Stego Image:")
+        extract_pge.layout().addRow(extract_stego_lbl)
+        extract_stego_ent = qtw.QLineEdit()
+        extract_stego_ent.setPlaceholderText("Enter the path to the stego image")
+        extract_stego_dialog = extract_stego_ent.addAction(browse_icon, extract_stego_ent.ActionPosition(1))
+        extract_stego_dialog.triggered.connect(lambda: get_path("extract_stego"))
+        extract_pge.layout().addRow(extract_stego_ent)
+
+        extract_txt_lbl = qtw.QLabel("Data File:")
+        extract_pge.layout().addRow(extract_txt_lbl)
+        extract_txt_ent = qtw.QLineEdit()
+        extract_txt_ent.setPlaceholderText("Enter the location to save the message")
+        extract_txt_dialog = extract_txt_ent.addAction(browse_icon, extract_txt_ent.ActionPosition(1))
+        extract_txt_dialog.triggered.connect(lambda: get_path("extract_txt"))
+        extract_pge.layout().addRow(extract_txt_ent)
+        # hide message submit
+        extract_btn = qtw.QPushButton("Submit", clicked=lambda: handle_submit("extract"))
+        extract_pge.layout().addRow(extract_btn)
+
+        # display
+        self.layout().addWidget(method_lbl)
+        self.layout().addWidget(mode_tab)
+        self.show()
+
+        def get_path(entry):
+            # TODO works on linux not sure about windows
+            start_dir = os.path.expanduser('~') + '/Downloads'
+            img_file_types = "Image Files (*.png *.jpg *.jpeg *.bmp *.gif *.tiff *.webp);;All Files (*)"
+            txt_file_types = "Image Files (*.txt);;All Files (*)"
+            match entry:
+                case 'hide_cover':
+                    path = qtw.QFileDialog.getOpenFileName(self, "Select The Cover Image to Hide The Data", start_dir, img_file_types)
+                    if path:
+                        hide_cover_ent.setText(path[0])
+                case 'hide_stego':
+                    path = qtw.QFileDialog.getSaveFileName(self, "Select The Location to save The Stego Image", start_dir, img_file_types)
+                    if path:
+                        hide_stego_ent.setText(path[0])
+                case 'hide_txt':
+                    path = qtw.QFileDialog.getOpenFileName(self, "Select The Message Text File", start_dir, txt_file_types)
+                    if path:
+                        hide_txt_ent.setText(path[0])
+                case 'extract_stego':
+                    path = qtw.QFileDialog.getOpenFileName(self, "Select The Stego Image to Extract", start_dir, img_file_types)
+                    if path:
+                        extract_stego_ent.setText(path[0])
+                case 'extract_txt':
+                    path = qtw.QFileDialog.getSaveFileName(self, "Select Location to Save The Message", start_dir, txt_file_types)
+                    if path:
+                        extract_txt_ent.setText(path[0])
 
 
-def handle_submit_click():
-    if mode.get() == 'embed':
-        input_img = input_img_entry.get()
-        output_img = output_img_entry.get()
-        input_txt = input_txt_entry.get()
-        lsb_embed = Popen(['python', 'embed.py', input_img, output_img, '-t', input_txt, '-f'],
-                          stdout=PIPE, stdin=PIPE, encoding='utf8')
-        responce = lsb_embed.stdout.readline()
-        match responce.split(',')[0]:
-            case 'Success':
-                messagebox.showinfo("Stegosaurus Success", responce)
-            case 'Error':
-                messagebox.showwarning("Stegosaurus Error", responce)
-            case 'Aborting':
-                messagebox.showwarning("Stegosaurus Error", responce)
-        # test_lbl = tk.Label(window, text=responce).pack()
-    elif mode.get() == 'decode':
-        input_img = input_img_entry.get()
-        output_txt = output_txt_entry.get()
-        lsb_decode = Popen(['python', 'decode.py', input_img, output_txt, '-f'],
-                           stdout=PIPE, stdin=PIPE, encoding='utf8')
-        responce = lsb_decode.stdout.readline()
-        match responce.split(',')[0]:
-            case 'Success':
-                messagebox.showinfo("Stegosaurus Success", responce)
-            case 'Error':
-                messagebox.showwarning("Stegosaurus Error", responce)
-            case _:
-                # TODO
-                messagebox.showinfo(";alskdf", "need to implement print")
-                # should just be for when message is printed
-                # new window to display output
-                # while responce:
-                #     output_lbl 
-                #     responce = lsb_decode.stdout.readline()
-        # test_lbl = tk.Label(window, text=responce).pack()
-    clear_text_feilds()
+
+        def handle_submit(mode):
+            stego_folder_path = sys.argv[0]
+            # path - 'stegosaurus.py'
+            stego_folder_path = stego_folder_path[0:len(stego_folder_path)-14]
+            match mode:
+                case 'hide_file':
+                    input_img = hide_cover_ent.text()
+                    output_img = hide_stego_ent.text()
+                    input_txt = hide_txt_ent.text()
+                    lsb_embed = Popen(['python', stego_folder_path + 'lsb_hide.py', input_img, output_img, '-t', input_txt, '-f'],
+                                      stdout=PIPE, encoding='utf8')
+                    responce = lsb_embed.stdout.readline()
+                    match responce.split(',')[0]:
+                        case 'Success':
+                            pop_up = qtw.QMessageBox()
+                            pop_up.setWindowTitle("Stegosaurus Success")
+                            pop_up.setIcon(qtw.QMessageBox.Icon.Information)
+                            pop_up.setText(responce)
+                            pop_up.exec()
+                        case 'Error':
+                            pop_up = qtw.QMessageBox()
+                            pop_up.setWindowTitle("Stegosaurus Error")
+                            pop_up.setIcon(qtw.QMessageBox.Icon.Critical)
+                            pop_up.setText(responce)
+                            pop_up.exec()
+                        case 'Aborting':
+                            pop_up = qtw.QMessageBox()
+                            pop_up.setWindowTitle("Stegosaurus Error")
+                            pop_up.setIcon(qtw.QMessageBox.Icon.Warning)
+                            pop_up.setText(responce)
+                            pop_up.exec()
+                    hide_cover_ent.setText("")
+                    hide_stego_ent.setText("")
+                    hide_txt_ent.setText("")
+                case 'extract':
+                    input_img = extract_stego_ent.text()
+                    output_txt = extract_txt_ent.text()
+                    lsb_decode = Popen(['python', stego_folder_path + 'lsb_extract.py', input_img, output_txt, '-f'],
+                                       stdout=PIPE, encoding='utf8')
+                    responce = lsb_decode.stdout.readline()
+                    match responce.split(',')[0]:
+                        case 'Success':
+                            pop_up = qtw.QMessageBox()
+                            pop_up.setWindowTitle("Stegosaurus Success")
+                            pop_up.setIcon(qtw.QMessageBox.Icon.Information)
+                            pop_up.setText(responce)
+                            pop_up.exec()
+                        case 'Error':
+                            pop_up = qtw.QMessageBox()
+                            pop_up.setWindowTitle("Stegosaurus Error")
+                            pop_up.setIcon(qtw.QMessageBox.Icon.Critical)
+                            pop_up.setText(responce)
+                            pop_up.exec()
+                        case _:
+                            # TODO
+                            pop_up = qtw.QMessageBox()
+                            pop_up.setWindowTitle("Stegosaurus Success")
+                            # should just be for when message is printed
+                            # new window to display output
+                            # while responce:
+                            #     output_lbl 
+                            #     responce = lsb_decode.stdout.readline()
+                    extract_stego_ent.setText("")
+                    extract_txt_ent.setText("")
 
 
-def clear_lsb_entry():
-    list = frame_file_input.grid_slaves()
-    for w in list:
-        w.grid_forget()
+app = qtw.QApplication([])
+window = Stegosaurus()
+app.exec()
 
-
-def display_lsb_embed():
-    mode.set('embed')
-    clear_lsb_entry()
-    # input image
-    input_img_label.grid(row=1, column=1, sticky="w")
-    input_img_entry.grid(row=2, column=1)
-    input_img_entry.delete(0, 'end')
-    input_img_btn.grid(row=2, column=2, padx=10)
-    # output image
-    output_img_label.grid(row=3, column=1, sticky="w")
-    output_img_entry.grid(row=4, column=1)
-    output_img_entry.delete(0, 'end')
-    output_img_btn.grid(row=4, column=2)
-    # input txt file
-    input_txt_label.grid(row=5, column=1, sticky="w")
-    input_txt_entry.grid(row=6, column=1)
-    input_txt_entry.delete(0, 'end')
-    input_txt_btn.grid(row=6, column=2)
-
-
-def display_lsb_decode():
-    mode.set('decode')
-    clear_lsb_entry()
-    # input image
-    input_img_label.grid(row=1, column=1, sticky="w")
-    input_img_entry.grid(row=2, column=1)
-    input_img_entry.delete(0, 'end')
-    input_img_btn.grid(row=2, column=2)
-    # output txt file
-    output_txt_label.grid(row=3, column=1, sticky="w")
-    output_txt_entry.grid(row=4, column=1)
-    output_txt_entry.delete(0, 'end')
-    output_txt_btn.grid(row=4, column=2)
-
-
-def open_img_file():
-    title = "Select The Input Image"
-    filetypes = (('Image Files', ('*.png', '*.jpg',
-                                  '*.jpeg', '*.bmp',
-                                  '*.gif', '*.tiff',
-                                  '*.webp')),
-                 ('All Files', '*'))
-    path = filedialog.askopenfilename(initialdir="~/Downloads", title=title,
-                                      filetypes=filetypes)
-    input_img_entry.insert(0, path)
-
-
-def save_img_file():
-    title = "Select The Output Image"
-    filetypes = (('Image Files', ('*.png', '*.jpg',
-                                  '*.jpeg', '*.bmp',
-                                  '*.gif', '*.tiff',
-                                  '*.webp')),
-                 ('All Files', '*'))
-    path = filedialog.asksaveasfilename(initialdir="~/Downloads", title=title,
-                                        filetypes=filetypes)
-    output_img_entry.insert(0, path)
-
-
-def open_txt_file():
-    title = "Select The Text File to Embed"
-    filetypes = (('Text Files', '*.txt'),
-                 ('All Files', '*'))
-    path = filedialog.askopenfilename(initialdir="~/Downloads", title=title,
-                                      filetypes=filetypes)
-    input_txt_entry.insert(0, path)
-
-
-def save_txt_file():
-    title = "Select The Location to Save The Decoded Data"
-    filetypes = (('Text Files', '*.txt'),
-                 ('All Files', '*'))
-    path = filedialog.asksaveasfilename(initialdir="~/Downloads", title=title,
-                                        filetypes=filetypes)
-    output_txt_entry.insert(0, path)
-
-
-# create stego program window
-window = tk.Tk()
-window.title("Stegosaurus")
-
-# global variables that will be assigned & input to command
-mode = tk.StringVar()
-output_print = tk.BooleanVar()
-
-# stegenography type title
-lbl_mode = tk.Label(text="LSB Stegenography")
-lbl_mode.pack(padx=10, pady=25)
-
-
-# create embed/decode mode buttons
-frame_mode = tk.Frame()
-frame_mode.pack()
-
-btn_lsb_embed = tk.Button(master=frame_mode, command=display_lsb_embed, text="Embed", width=15)
-btn_lsb_embed.grid(row=0, column=0, padx=10, ipadx=10)
-
-btn_lsb_decode = tk.Button(master=frame_mode, command=display_lsb_decode, text="Decode", width=15)
-btn_lsb_decode.grid(row=0, column=1, padx=10, ipadx=10)
-
-
-# create all types of input
-frame_file_input = tk.Frame()
-frame_file_input.pack()
-
-# input feilds
-# ------------
-# input image
-input_img_label = tk.Label(master=frame_file_input, text="Input Image")
-input_img_entry = tk.Entry(master=frame_file_input, width=50)
-input_img_btn = tk.Button(master=frame_file_input, command=open_img_file, text="Search")
-
-# output image
-output_img_label = tk.Label(master=frame_file_input, text="Output Image")
-output_img_entry = tk.Entry(master=frame_file_input, width=50)
-output_img_btn = tk.Button(master=frame_file_input, command=save_img_file, text="Search")
-
-# input text file
-input_txt_label = tk.Label(master=frame_file_input, text="Input Txt File")
-input_txt_entry = tk.Entry(master=frame_file_input, width=50)
-input_txt_btn = tk.Button(master=frame_file_input, command=open_txt_file, text="Search")
-
-# input string
-input_string_label = tk.Label(master=frame_file_input, text="Input String")
-input_string_entry = tk.Entry(master=frame_file_input, width=50)
-
-# output text file
-output_txt_label = tk.Label(master=frame_file_input, text="Output Txt File")
-output_txt_entry = tk.Entry(master=frame_file_input, width=50)
-output_txt_btn = tk.Button(master=frame_file_input, command=save_txt_file, text="Search")
-
-
-# Create a new frame `frame_buttons` to contain the
-# Submit and Clear buttons. This frame fills the
-frame_buttons = tk.Frame()
-frame_buttons.pack(fill=tk.X, ipadx=5, ipady=5)
-
-# Create the "Submit" button and pack it to the
-btn_submit = tk.Button(master=frame_buttons, command=handle_submit_click, text="Submit")
-btn_submit.pack(padx=10, ipadx=10)
-
-
-# Start the application
-display_lsb_embed()
-window.mainloop()
