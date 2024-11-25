d7c1b7d7c1495f6d391ba1359196a6b446a6c586
Emory Grubbs <emoryjgrubbs@gmail.com>
2024-11-25 01:39:30 -0600

    added functionaility for the gui to take in a string/print directly

--- stegosaurus.py
+++ stegosaurus.py
@@ -30,6 +30,7 @@ class Stegosaurus(qtw.QWidget):
         # hide mode page
         hide_pge = qtw.QWidget(mode_tab)
         hide_pge.setLayout(qtw.QFormLayout())
+        hide_pge.layout().setHorizontalSpacing(25)
         mode_tab.addTab(hide_pge, "Hide Message")
         # hide message inputs
         hide_cover_lbl = qtw.QLabel("Cover Image:")
@@ -48,16 +49,19 @@ class Stegosaurus(qtw.QWidget):
         hide_stego_dialog.triggered.connect(lambda: get_path("hide_stego"))
         hide_pge.layout().addRow(hide_stego_ent)
 
-        hide_txt_lbl = qtw.QLabel("Message File:")
-        hide_pge.layout().addRow(hide_txt_lbl)
+        hide_msg_lbl = qtw.QLabel("Message File:")
+        hide_msg_chk = qtw.QCheckBox("Hide String?", clicked=lambda: switch_hide_ent())
+        hide_pge.layout().addRow(hide_msg_lbl, hide_msg_chk)
         hide_txt_ent = qtw.QLineEdit()
         hide_txt_ent.setPlaceholderText("Enter the path to the message text file")
         hide_txt_dialog = hide_txt_ent.addAction(browse_icon, hide_txt_ent.ActionPosition(1))
         hide_txt_dialog.triggered.connect(lambda: get_path("hide_txt"))
         hide_pge.layout().addRow(hide_txt_ent)
-
         hide_str_ent = qtw.QTextEdit()
         hide_str_ent.setPlaceholderText("Enter the message to hide")
+        hide_pge.layout().addRow(hide_str_ent)
+        hide_pge.layout().setRowVisible(hide_str_ent, False)
+
         # hide message submit
         hide_btn = qtw.QPushButton("Submit", clicked=lambda: handle_submit("hide_file"))
         hide_pge.layout().addRow(hide_btn)
@@ -65,6 +69,7 @@ class Stegosaurus(qtw.QWidget):
         # extract mode page
         extract_pge = qtw.QWidget(mode_tab)
         extract_pge.setLayout(qtw.QFormLayout())
+        extract_pge.layout().setHorizontalSpacing(25)
         mode_tab.addTab(extract_pge, "Extract Message")
         # extract message inputs
         extract_stego_lbl = qtw.QLabel("Stego Image:")
@@ -76,7 +81,8 @@ class Stegosaurus(qtw.QWidget):
         extract_pge.layout().addRow(extract_stego_ent)
 
         extract_txt_lbl = qtw.QLabel("Data File:")
-        extract_pge.layout().addRow(extract_txt_lbl)
+        extract_out_chk = qtw.QCheckBox("Print Message?", clicked=lambda: switch_extract_out())
+        extract_pge.layout().addRow(extract_txt_lbl, extract_out_chk)
         extract_txt_ent = qtw.QLineEdit()
         extract_txt_ent.setPlaceholderText("Enter the location to save the message")
         extract_txt_dialog = extract_txt_ent.addAction(browse_icon, extract_txt_ent.ActionPosition(1))
@@ -90,6 +96,7 @@ class Stegosaurus(qtw.QWidget):
         self.layout().addWidget(method_lbl)
         self.layout().addWidget(mode_tab)
         self.show()
+
 
         def get_path(entry):
             # TODO works on linux not sure about windows
@@ -119,6 +126,25 @@ class Stegosaurus(qtw.QWidget):
                         extract_txt_ent.setText(path[0])
 
 
+        def switch_hide_ent():
+            if hide_msg_chk.checkState() == Qt.CheckState.Checked:
+                hide_pge.layout().setRowVisible(hide_txt_ent, False)
+                hide_pge.layout().setRowVisible(hide_str_ent, True)
+                hide_msg_lbl.setText("Message String:")
+            else:
+                hide_pge.layout().setRowVisible(hide_str_ent, False)
+                hide_pge.layout().setRowVisible(hide_txt_ent, True)
+                hide_msg_lbl.setText("Message File:")
+
+
+        def switch_extract_out():
+            if extract_out_chk.checkState() == Qt.CheckState.Checked:
+                extract_txt_ent.setDisabled(True)
+                extract_txt_dialog.setDisabled(True)
+            else:
+                extract_txt_ent.setEnabled(True)
+                extract_txt_dialog.setEnabled(True)
+
 
         def handle_submit(mode):
             stego_folder_path = sys.argv[0]
@@ -129,8 +155,13 @@ class Stegosaurus(qtw.QWidget):
                     input_img = hide_cover_ent.text()
                     output_img = hide_stego_ent.text()
                     input_txt = hide_txt_ent.text()
-                    lsb_embed = Popen(['python', stego_folder_path + 'lsb_hide.py', input_img, output_img, '-t', input_txt, '-f'],
-                                      stdout=PIPE, encoding='utf8')
+                    input_str = hide_str_ent.toPlainText()
+                    if hide_msg_chk.checkState() == Qt.CheckState.Checked:
+                        lsb_embed = Popen(['python', stego_folder_path + 'lsb_hide.py', input_img, output_img, '-s', input_str, '-f'],
+                                          stdout=PIPE, encoding='utf8')
+                    else:
+                        lsb_embed = Popen(['python', stego_folder_path + 'lsb_hide.py', input_img, output_img, '-t', input_txt, '-f'],
+                                          stdout=PIPE, encoding='utf8')
                     responce = lsb_embed.stdout.readline()
                     match responce.split(',')[0]:
                         case 'Success':
@@ -154,11 +185,16 @@ class Stegosaurus(qtw.QWidget):
                     hide_cover_ent.setText("")
                     hide_stego_ent.setText("")
                     hide_txt_ent.setText("")
+                    hide_str_ent.setText("")
                 case 'extract':
                     input_img = extract_stego_ent.text()
                     output_txt = extract_txt_ent.text()
-                    lsb_decode = Popen(['python', stego_folder_path + 'lsb_extract.py', input_img, output_txt, '-f'],
-                                       stdout=PIPE, encoding='utf8')
+                    if extract_out_chk.checkState() == Qt.CheckState.Checked:
+                        lsb_decode = Popen(['python', stego_folder_path + 'lsb_extract.py', input_img, '-p', '-f'],
+                                           stdout=PIPE, encoding='utf8')
+                    else:
+                        lsb_decode = Popen(['python', stego_folder_path + 'lsb_extract.py', input_img, output_txt, '-f'],
+                                           stdout=PIPE, encoding='utf8')
                     responce = lsb_decode.stdout.readline()
                     match responce.split(',')[0]:
                         case 'Success':
@@ -177,6 +213,10 @@ class Stegosaurus(qtw.QWidget):
                             # TODO
                             pop_up = qtw.QMessageBox()
                             pop_up.setWindowTitle("Stegosaurus Success")
+                            pop_up.setIcon(qtw.QMessageBox.Icon.Information)
+                            pop_up.setText("The Message is: ")
+                            pop_up.setText(responce)
+                            pop_up.exec()
                             # should just be for when message is printed
                             # new window to display output
                             # while responce:
